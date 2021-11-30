0c95a0d63f43399c14fa836f16f0799d0c64d8af
SergeySidorchuk <serg_ss@ukr.net>
2021-12-01 00:34:18 +0200

    added 1.js and 2.js

--- 1.js
+++ 1.js
@@ -1,25 +1,66 @@
-// function palindrom(str) {
-//   // Приводим к нижнему регистру
-//   str = str.toLowerCase();
-//   return str === str.split("").reverse().join("");
-//   //   // Приводим сторку  в массив
-//   //   let str2 = str.split("");
-//   //   // Зазворачиваем строку
-//   //   str2 = str2.reverse();
-//   //   // Обьеденяем  строку
-//   //   str2 = str2.join();
-//   //   if (str === str2) return true;
-//   //   else return false;
-//   //   return str2;
-// }
-// console.log(palindrom("hello"));
-// console.log(palindrom("abba"));
+// 1) Паше 17 лет. Пока он еще ребенок, но через год станет полноценной ячейкой общества. 
+// Напишите скрипт, который бы показывал, что пока Паше 17 лет, 
+// он - ребенок, но, когда станет 18 - он взрослый. 
+// Возраст Паши поместите в переменную."
 
-const palindrom = (str) => {
-  str = str.toLowerCase();
-  return str === str.split("").reverse().join("");
-};
+const age = 17;
+if(age < 18){
+  console.log('Ребенок');
+}
+else{
+  console.log(' Не ребенок');
 
-console.log(palindrom("tippit"));
-console.log(palindrom("word"));
-console.log(palindrom("palindrom"));
+}
+
+// 2) Дана строка 'Гуляю по городу и вижу солнце'.
+//  Сделайте так, чтобы каждое слово было отдельным элементом массива.
+// Должно получиться: ["Гуляю","по","городу", "и", "вижу", "cолнце"];
+
+let string = 'Гуляю по городу и вижу солнце';
+console.log(string.split(' '));
+
+// 3) Напишите функцию, которая делает каждую первую букву в предложении 
+// 'я тебя очень люблю и ценю' Заглавной.",
+
+let str2 = 'я тебя очень люблю и ценю';
+console.log(str2.split(/\s+/)
+.map(word => word[0]
+.toUpperCase() 
++ word.substring(1))
+.join(' '));
+
+// 4) Вам дан массив брендов машин:
+//  ['BMW', 'Mersedes', 'Opel', 'Mazda', 'FIAT', 'Ferarri', 'Reno'].
+  // Напишите функцию, которая принимает данный массив в качестве параметра
+  //  и возвращает массив только с теми брендами,
+    // название которых состоит из заглавных букв.
+
+const arr = ['BMW', 'Mersedes', 'Opel', 'Mazda', 'FIAT', 'Ferarri', 'Reno'];
+function cars (arr){
+let newArr = [];    // создаем новый массив, в который будем пушить результат //
+for(let i = 0; i < arr.length; i++) {   // счетчик //
+  let upperElement = arr[i].toUpperCase()  // записываем в переменную элементы массива и приводим их в верхний регистр//
+  if(upperElement === arr[i]) { // сравниваем элементы массива и созданную переменную //
+    newArr.push(arr[i]); // пушим в новый массив элементы в верхнем регистре//
+  }
+}
+return newArr 
+} 
+console.log(cars(arr));
+
+
+
+
+// 5) Дана строка: 'abcde'. Проверьте, что первым символом этой строки является буква
+//  'a'. Если это так - выведите 'да',
+//   в противном случае выведите 'нет'.
+let string1 = "abcde";
+let firstLetter = string1.charAt(0); // Получаем первую букву строки
+
+if(firstLetter === 'a'){ // Сравниваем ее с "а"
+  console.log('Да');
+}
+else {
+  console.log('Нет');
+}
+
--- 2.js
+++ 2.js
@@ -1,123 +1,59 @@
-// let chess = [
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-//   [0, 0, 0, 0, 0, 0, 0, 0],
-// ];
+// 1) Напишите функцию, которая принимает в себя год и возвращает век, в котором он находится.
+// Пример:
+//         "centry(1705) => return 18",
+//         "centry(1601) => return 17",
+//         "centry(1900) => return 19",
+//         "centry(81) => return 1",
+function change(year) {
+if (year/100 === 0) return 1;  // Если год 0 или меньше 1 возрвщаем 1
+else if(year % 100 === 0) return year/100; // Если остаток от деления 0, то возвращаем год/100
+else return Math.trunc(year/100) + 1; // Убираем цифры после запятой и добавляем 1
+}
+console.log(change(1705));
+console.log(change(1900));
+console.log(change(81));
 
-// function draw() {
-//   let out = "";
-//   let m = 0;
 
-//   for (let i = 0; i < chess.length; i++) {
-//     let arr = chess[i];
-//     for (let k = 0; k < arr.length; k++) {
-//       if (m % 2 === 0) {
-//         out += `<div class="chess-block" data-x="${k}" data-y="${i}"></div>`;
-//       } else {
-//         out += `<div class="chess-block bg-black"  data-x="${k}" data-y="${i}"></div>`;
-//       }
-//       m++;
-//     }
-//     m++;
-//   }
-//   document.querySelector("#field").innerHTML = out;
-//   document.querySelectorAll(".chess-block").forEach(function (element) {
-//     element.onclick = horse;
-//   });
+// 2) Напишите функцию, которая будет принимать в себя текущий цвет светофора,
+//  а возвращать цвет, в который он должен перейти.
+//   Учитывайте то, что после красного, всегда загорается зеленый, а после зеленого желтый, а затем красный.
+// Пример:
+//         "updateLight('Зелёный') => 'Жёлтый'",
+//         "updateLight('Жёлтый') => 'Красный'",
+//         "updateLight('Красный') => 'Зелёный'",
+function updateLight (currentColor) {
+switch(currentColor) {
+  case 'Зелёный': return 'Жёлтый';     // Если value = Зелёный, тогда Жёлтый  
+  case 'Жёлтый': return 'Красный';     // Если value = Жёлтый, тогда Красный  
+    case 'Красный': return 'Зелёный';  // Если value = Красный, тогда Зелёный  
+    default: return 'Error'; 
+}
+}
+console.log(updateLight('Зелёный'));
+console.log(updateLight('Жёлтый'));
+console.log(updateLight('Красный'));
+console.log(updateLight('Синий'));
+
+
+
+// // 3) Напишите функцию, которая принимает в себя массив, а возвращает произведение элементов массива,
+//  умноженных по порядку друг на друга.
+// Пример:
+//         "example([1, 2, 3, 4]) => 24"
+
+// function example (arr){
+// let arr1=[]
+
 // }
 
-// draw();
+let mult = 1;
 
-// function horse() {
-//   document.querySelectorAll(".chess-block").forEach(function (e) {
-//     e.classList.remove("green");
-//     e.classList.remove("active");
-//   });
-//   let x = this.dataset.x;
-//   let y = this.dataset.y;
-//   this.classList.add("green");
-//   if (+x + 2 < 8 && +y + 1 < 8) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x + 2}"][data-y="${+y + 1}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x + 2 < 8 && +y - 1 >= 0) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x + 2}"][data-y="${+y - 1}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x - 2 >= 0 && +y + 1 < 8) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x - 2}"][data-y="${+y + 1}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x - 2 >= 0 && +y - 1 >= 0) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x - 2}"][data-y="${+y - 1}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x + 1 < 8 && +y - 2 >= 0) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x + 1}"][data-y="${+y - 2}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x - 2 >= 0 && +y - 2 >= 0) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x - 1}"][data-y="${+y - 2}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x + 1 < 8 && +y + 2 < 8) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x + 1}"][data-y="${+y + 2}"]`)
-//       .classList.add("active");
-//   }
-//   if (+x - 2 >= 0 && +y + 2 < 8) {
-//     document
-//       .querySelector(`.chess-block[data-x="${+x - 1}"][data-y="${+y + 2}"]`)
-//       .classList.add("active");
-//   }
-// }
-
-function draw() {
-  for (let i = 0; i < 8; i++) {
-    for (let k = 0; k < 8; k++) {
-      let out = '<div class="chess-block';
-      if ((i + k) % 2 === 0) {
-        out += " bg-black";
-      }
-      document.querySelector("#field").innerHTML +=
-        out + '" data-x="' + k + '" data-y="' + i + '"></div>';
-    }
+let example = (arr) => {
+for (let i of arr) {
+  // console.log(i);
+    mult *= i;
+    console.log(mult);
   }
-  document.querySelectorAll(".chess-block").forEach(function (element) {
-    element.onclick = horse;
-  });
+  return mult;
 }
-
-draw();
-
-function horse() {
-  const dx = [2, 1, -1, -2, -2, -1, 1, 2],
-    dy = [1, 2, 2, 1, -1, -2, -2, -1];
-  document.querySelectorAll(".chess-block").forEach(function (element) {
-    element.classList.remove("active");
-    element.classList.remove("green");
-  });
-  let x = +this.dataset.x,
-    y = +this.dataset.y;
-  this.classList.add("green");
-  for (let i = 0; i < 8; i++) {
-    let xx = x + dx[i],
-      yy = y + dy[i];
-    if (xx >= 0 && yy >= 0 && xx < 8 && yy < 8) {
-      document
-        .querySelector('.chess-block[data-x="' + xx + '"][data-y="' + yy + '"]')
-        .classList.add("active");
-    }
-  }
-}
+console.log(example([5,7,10]));
